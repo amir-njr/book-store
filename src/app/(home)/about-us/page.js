b604bbdbc0671aa9a712dc84fba94f075d11587a
amir-njr <amirr.najjar.78@gmail.com>
2023-09-06 01:02:04 +0330

    Create About Us Page

---
+++ src/app/(home)/about-us/page.js
@@ -0,0 +1,11 @@
+import AboutUs from "@/components/template/AboutUs/AboutUs";
+
+const aboutUs = () => {
+    return (
+        <div>
+            <AboutUs />
+        </div>
+    );
+};
+
+export default aboutUs;
---
+++ src/app/(home)/contact-us/page.js
@@ -0,0 +1,11 @@
+import ContactUs from "@/components/template/ContactUs/ContactUs";
+
+const contactUs = () => {
+  return (
+    <div>
+      <ContactUs />
+    </div>
+  );
+};
+
+export default contactUs;
